4a3b2c713bdbb3ccfcbab9d6625c14f2be7b88ed
AndewSteve <2535023693@qq.com>
2023-09-03 23:49:54 +0800

    [刘晔华]实现每个对象的本地数据连接
    现在图片路径用硬代码的形式与棋子类型挂钩

--- Managed/ChessModel/士.py
+++ Managed/ChessModel/士.py
@@ -1,4 +1,17 @@
-from .Chess import Chess
+import os,pygame
+from .Chess import Chess,ChessColor,chess_img_path
+from Managed.Game import scale_img
 class 士(Chess):
+    def init(self, position):
+        if(self.color==ChessColor.RED):
+            self.chess_img_path = os.path.join(chess_img_path,"士.png")
+        else:
+            self.chess_img_path = os.path.join(chess_img_path,"仕.png")
+        self.image = pygame.image.load(self.chess_img_path)
+        self.image = scale_img(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.center = position
+        self.x,self.y = position
+        
     def onSelected(self):
         return super().onSelected()
